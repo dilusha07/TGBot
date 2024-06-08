3221b793b42fcd48ce77c401a3affb8c64e2c68c
dilusha07 <dilushasandaruwani07@gmail.com>
2024-06-08 22:54:24 +0530

    Initialize the Project & Install Dependencies

---
+++ main.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello Bot!');
+});
+
+
+
+//Server port
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
