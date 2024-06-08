73b351961b485a53425329aff84947ae54784983
dilusha07 <dilushasandaruwani07@gmail.com>
2024-06-09 00:38:09 +0530

    Add bot commands

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const express = require('express');
+const { Telegraf } = require('telegraf');
 
-const app = express();
-const port = 3001;
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-app.get('/', (req, res) => {
-  res.send('Hello Bot!');
-});
+bot.start((ctx) => ctx.reply('Welcome to the TGBot!'));
+
+bot.register((ctx) => ctx.reply('Initiates the ticket registration process'));
+
+bot.help((ctx) => ctx.reply('Bot Commands:\n/start - Greets the user and provides event details\n/stop - Stop the bot\n/register -  Initiates the ticket registration process.\n/help - Provides help and usage instructions.'));
+
+bot.launch();
 
 
 
-//Server port
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// const express = require('express');
+
+// const app = express();
+// const port = 3001;
+// app.get('/', (req, res) => {
+//   res.send('Hello Bot!');
+// });
+
+
+// //Server port
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}`);
+// });
